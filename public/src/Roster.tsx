2071ec308960542564f5b2ad958cfad6aa1198d3
Scott Odle <scott@sjodle.com>
2019-01-04 16:28:25 -0700

    style the roster page

--- public/src/Roster.tsx
+++ public/src/Roster.tsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { RouteComponentProps } from 'react-router';
 import app from './base';
+import Container from 'reactstrap/lib/Container';
+import Row from 'reactstrap/lib/Row';
+import Col from 'reactstrap/lib/Col';
+import Button from 'reactstrap/lib/Button';
 
 interface ICourseListing {
     [key: string]: Array<string>
@@ -40,48 +44,65 @@ export default class Roster extends Component<RouteComponentProps<IMatchProps>> 
         const rosterId = this.props.match.params.id;
         if (this.state.user.classes.hasOwnProperty(rosterId)) {
             return (
-                <div>
-                    <h1>Class Roster: {rosterId}</h1>
-                    <button onClick={() => {
-                        let classes = Object.assign({}, this.state.user.classes);
-                        let name = '';
-                        while (name === '') {
-                            let newName = prompt('Student name?');
-                            if (newName === null) {
-                                return;
-                            } else {
-                                name = newName;
-                            }
+                <Container>
+                    <Row>
+                        <Col xs='12'>
+                            <h1>Class Roster: {rosterId}</h1>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md='6'>
+                            <Button color='primary' onClick={() => {
+                                let classes = Object.assign({}, this.state.user.classes);
+                                let name = '';
+                                while (name === '') {
+                                    let newName = prompt('Student name?');
+                                    if (newName === null) {
+                                        return;
+                                    } else {
+                                        name = newName;
+                                    }
+                                }
+                                classes[rosterId].push(name);
+                                this.ref.update({classes});
+                            }}>+ Add a Student</Button>
+                        </Col>
+                        <Col md='6'>
+                            <Button color='secondary' onClick={() => {
+                                if (this.state.user.classes[rosterId].length === 0) {
+                                    alert('You must add a student first.');
+                                } else {
+                                    let students = this.state.user.classes[rosterId];
+                                    alert(students[Math.floor(Math.random() * students.length)]);
+                                }
+                            }}>Pick Randomly</Button>
+                        </Col>
+                    </Row>
+                    <Row>
+                        {
+                            (this.state.user.classes[rosterId].length > 0) ?
+                                <Col xs='12'>
+                                    {this.state.user.classes[rosterId].sort().map((s, i) => {
+                                        return <Row key={i}>
+                                            <Col xs='6'>
+                                                <h2>{s}</h2>
+                                            </Col>
+                                            <Col xs='6'>
+                                                <Button color='danger' onClick={() => {
+                                                    if (confirm(`Really delete ${s}?`)) {
+                                                        let classes = Object.assign({}, this.state.user.classes);
+                                                        classes[rosterId].splice(i, 1);
+                                                        this.ref.update({classes});
+                                                    }
+                                                }}>Delete</Button>
+                                            </Col>
+                                        </Row>;
+                                    })}
+                                </Col> :
+                                <Col xs='12'>No students yet. Add one!</Col>
                         }
-                        classes[rosterId].push(name);
-                        this.ref.update({classes});
-                    }}>+ Add a Student</button>
-                    <button onClick={() => {
-                        if (this.state.user.classes[rosterId].length === 0) {
-                            alert('You must add a student first.');
-                        } else {
-                            let students = this.state.user.classes[rosterId];
-                            alert(students[Math.floor(Math.random() * students.length)]);
-                        }
-                    }}>Pick Randomly</button>
-                    {
-                        (this.state.user.classes[rosterId].length > 0) ?
-                            <div>
-                                {this.state.user.classes[rosterId].sort().map((s, i) => {
-                                    return <div key={i}>
-                                        {s} <button onClick={() => {
-                                            if (confirm(`Really delete ${s}?`)) {
-                                                let classes = Object.assign({}, this.state.user.classes);
-                                                classes[rosterId].splice(i, 1);
-                                                this.ref.update({classes});
-                                            }
-                                        }}>Delete</button>
-                                    </div>;
-                                })}
-                            </div> :
-                            <div>No students yet. Add one!</div>
-                    }
-                </div>
+                    </Row>
+                </Container>
             );
         } else {
             return <div>Class {rosterId} not found!</div>;
