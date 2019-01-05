b39dbdd70e6f67488ea1c040912e2c34fb059896
Scott Odle <scott@sjodle.com>
2019-01-04 18:44:17 -0700

    add home cta

--- public/src/Home.tsx
+++ public/src/Home.tsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import app from './base';
+import Container from 'reactstrap/lib/Container';
+import Alert from 'reactstrap/lib/Alert';
+import Row from 'reactstrap/lib/Row';
+import Col from 'reactstrap/lib/Col';
 
 interface IHomeState {
     user: firebase.User | null
@@ -9,16 +13,66 @@ export default class Home extends Component {
     state: IHomeState = {
         user: null
     };
+    nameList = [
+        'Robin',
+        'Roger',
+        'Rodolfo',
+        'Jose',
+        'Casey',
+        'Jerome',
+        'Roderick',
+        'Grady',
+        'Emmett',
+        'Wayne',
+        'Jody',
+        'Olivia',
+        'Angelica',
+        'Myra',
+        'Fannie',
+        'Hope',
+        'Andrea',
+        'Katrina',
+        'Samantha',
+        'Teresa'
+    ]
     render() {
         return (
-            <div>
-                <p>Hi!</p>
-                {
-                    (this.state.user !== null) ? 
-                        <p>Logged in as {this.state.user!.displayName}. <a href='/logout'>Log out</a></p> :
-                        <p><a href='/login'>Log in!</a></p>
-                }
-            </div>
+            <Container>
+                <Row>
+                    <Col xs='12'>
+                        {
+                            (this.state.user !== null) ?
+                                <Alert color="success">Logged in as {this.state.user.displayName}</Alert> :
+                                <Alert color="secondary">Not logged in - <a href='/login'>Sign in with Google</a></Alert>
+                        }
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs='12'>
+                        <h2>Increase student engagement</h2>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md='4'>
+                        <h3>Upload your class list</h3>
+                        <p>
+                            You can organize by class period.
+                        </p>
+                    </Col>
+                    <Col md='4'>
+                        <h3>Connect your device</h3>
+                        <p>
+                            Alexa, Google Assistant, and Slack integration coming soon.
+                        </p>
+                    </Col>
+                    <Col md='4'>
+                        <h3>"Alexa, open Random Student"</h3>
+                        <p>
+                            "I think it's {this.nameList[Math.floor(Math.random() * this.nameList.length)]}'s turn to answer."
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 
