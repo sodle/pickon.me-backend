0e25e56feba21dd72873a0f73648d4a2f967acd0
Scott Odle <scott@sjodle.com>
2019-01-22 05:58:11 -0700

    rebrand

--- public/src/Privacy.tsx
+++ public/src/Privacy.tsx
@@ -13,9 +13,9 @@ export default class Privacy extends Component {
                 <p>Effective date: January 05, 2019</p>
                 
                 
-                <p>Random Student Picker for Alexa ("us", "we", or "our") operates the https://randomstudent-ba994.firebaseapp.com website (the "Service").</p>
+                <p>PickOn.Me ("us", "we", or "our") operates the https://randomstudent-ba994.firebaseapp.com website (the "Service").</p>
                 
-                <p>This page informs you of our policies regarding the collection, use, and disclosure of personal data when you use our Service and the choices you have associated with that data. Our Privacy Policy  for Random Student Picker for Alexa is based on the <a href="https://www.freeprivacypolicy.com/blog/sample-privacy-policy-template/">Free Privacy Policy Template Website</a>.</p>
+                <p>This page informs you of our policies regarding the collection, use, and disclosure of personal data when you use our Service and the choices you have associated with that data. Our Privacy Policy  for PickOn.Me is based on the <a href="https://www.freeprivacypolicy.com/blog/sample-privacy-policy-template/">Free Privacy Policy Template Website</a>.</p>
                 
                 <p>We use your data to provide and improve the Service. By using the Service, you agree to the collection and use of information in accordance with this policy. Unless otherwise defined in this Privacy Policy, terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, accessible from https://randomstudent-ba994.firebaseapp.com</p>
                 
@@ -51,7 +51,7 @@ export default class Privacy extends Component {
                 
                 <h2>Use of Data</h2>
                     
-                <p>Random Student Picker for Alexa uses the collected data for various purposes:</p>    
+                <p>PickOn.Me uses the collected data for various purposes:</p>    
                 <ul>
                     <li>To provide and maintain the Service</li>
                     <li>To notify you about changes to our Service</li>
@@ -66,15 +66,15 @@ export default class Privacy extends Component {
                 <p>Your information, including Personal Data, may be transferred to — and maintained on — computers located outside of your state, province, country or other governmental jurisdiction where the data protection laws may differ than those from your jurisdiction.</p>
                 <p>If you are located outside United States and choose to provide information to us, please note that we transfer the data, including Personal Data, to United States and process it there.</p>
                 <p>Your consent to this Privacy Policy followed by your submission of such information represents your agreement to that transfer.</p>
-                <p>Random Student Picker for Alexa will take all steps reasonably necessary to ensure that your data is treated securely and in accordance with this Privacy Policy and no transfer of your Personal Data will take place to an organization or a country unless there are adequate controls in place including the security of your data and other personal information.</p>
+                <p>PickOn.Me will take all steps reasonably necessary to ensure that your data is treated securely and in accordance with this Privacy Policy and no transfer of your Personal Data will take place to an organization or a country unless there are adequate controls in place including the security of your data and other personal information.</p>
                 
                 <h2>Disclosure Of Data</h2>
                 
                 <h3>Legal Requirements</h3>
-                <p>Random Student Picker for Alexa may disclose your Personal Data in the good faith belief that such action is necessary to:</p>
+                <p>PickOn.Me may disclose your Personal Data in the good faith belief that such action is necessary to:</p>
                 <ul>
                     <li>To comply with a legal obligation</li>
-                    <li>To protect and defend the rights or property of Random Student Picker for Alexa</li>
+                    <li>To protect and defend the rights or property of PickOn.Me</li>
                     <li>To prevent or investigate possible wrongdoing in connection with the Service</li>
                     <li>To protect the personal safety of users of the Service or the public</li>
                     <li>To protect against legal liability</li>
