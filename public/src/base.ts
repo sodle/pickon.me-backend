11f7abcec52afc7ea45fd85462d570a1a7be09a1
Scott Odle <scott@sjodle.com>
2019-01-23 21:38:27 -0700

    update signin app name

--- public/src/base.ts
+++ public/src/base.ts
@@ -2,7 +2,7 @@ import firebase from 'firebase/app';
 
 const app = firebase.initializeApp({
     apiKey: "AIzaSyBpxh4y-AnXBXyefFdwIl-M603mwJpqGuA",
-    authDomain: "randomstudent-ba994.firebaseapp.com",
+    authDomain: "PickOn.Me",
     databaseURL: "https://randomstudent-ba994.firebaseio.com",
     projectId: "randomstudent-ba994",
     storageBucket: "randomstudent-ba994.appspot.com",
